769e08f4cf84c27513be4d7f86c244c3240b5d12
Arawn920 <kayra1290@gmail.com>
2022-06-22 16:28:37 +0300

    return yazıldı

--- OnlineShopping/Areas/Admin/Controllers/HomeController.cs
+++ OnlineShopping/Areas/Admin/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace OnlineShopping.Areas.Admin.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(); //return
         }
     }
 }
